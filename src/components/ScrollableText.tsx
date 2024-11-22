21bd6ed585ce9e8a0ea2591b4180db19837af7ae
Momen-Assaf <momenssf@gmail.com>
2024-11-22 20:13:58 +0200

    Scrollable Text

---
+++ src/components/ScrollableText.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ScrollableText = ({
+    text = "Click Me", // Default text
+    targetSectionId = "default-section", // Default target section id
+}) => {
+    const handleClick = () => {
+        if (targetSectionId) {
+            const section = document.getElementById(targetSectionId);
+            if (section) {
+                section.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    };
+
+    return (
+        <span
+            className="text-gray-800 cursor-pointer transition-colors hover:text-[#1E407D]"
+            onClick={handleClick}
+        >
+            {text}
+        </span>
+    );
+};
+
+export default ScrollableText;
