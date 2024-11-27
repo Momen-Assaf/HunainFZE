fdc3638a3f8e5aab7f728e9be8515a627fcdcb62
Momen-Assaf <momenssf@gmail.com>
2024-11-27 02:04:06 +0200

    hero section allignment

--- src/pages/Landing/components/HeroSection.tsx
+++ src/pages/Landing/components/HeroSection.tsx
@@ -1,8 +1,8 @@
 import theme from "../../../theme";
 import CustomButton from "../../../components/CustomButton";
+
 const HeroSection = () => {
   return (
-    // className="  flex items-center"
     <section className="container mx-auto flex items-center justify-center space-x-[100px]">
       {/* Left Section */}
       <div className="flex-1 text-left pl-4">
@@ -13,22 +13,26 @@ const HeroSection = () => {
             fontSize: "96px",
           }}
           className="leading-tight font-medium"
-          >
-          From Idea to{" "}
-          <span
-            style={{
-              color: theme.colors.primary,
-              fontWeight: theme.fontWeight.bold,
-            }}
+        >
+          <div>From Idea</div>
+          <div>
+            to{" "}
+            <span
+              style={{
+                color: theme.colors.primary,
+                fontWeight: theme.fontWeight.bold,
+              }}
             >
-            Delivery
-          </span>
+              Delivery
+            </span>
+          </div>
         </h1>
       </div>
 
       {/* Right Section */}
       <div className="flex-1 flex flex-col">
         <p
+          className="text-start pb-[35px]"
           style={{
             fontFamily: theme.fonts.primary,
             color: theme.colors.textDark,
