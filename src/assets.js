8aca3a57808c9d0569cf100622d4a2d2801c3373
Momen-Assaf <momenssf@gmail.com>
2024-11-27 15:35:38 +0200

    Assets added

--- src/assets.js
+++ src/assets.js
@@ -1,6 +1,10 @@
 /// Images
 export const logo = "/assets/images/logo.png";
 export const effect = "/assets/images/Effect.svg";
+export const vrZoneLogo = "/assets/images/vr_zone_logo.svg";
+export const vrZoneHover = "/assets/images/vr_zone_hover.svg";
+export const blueBirdLogo = "/assets/images/blue_bird_logo.svg";
+export const blueBirdHover = "/assets/images/blue_bird_hover.svg";
 
 /// Icons
 export const arrowIcon = "/assets/images/icons/arrow_right.svg";
