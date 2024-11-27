9b766f2562c391922962ec25f450e42f7e81ab36
Momen-Assaf <momenssf@gmail.com>
2024-11-27 13:03:42 +0200

    assets added

--- src/assets.js
+++ src/assets.js
@@ -1,3 +1,9 @@
+/// Images
 export const logo = "/assets/images/logo.png";
-export const arrowIcon = "/assets/images/icons/arrow_right.svg";
 export const effect = "/assets/images/Effect.svg";
+
+/// Icons
+export const arrowIcon = "/assets/images/icons/arrow_right.svg";
+export const codeIcon = "/assets/images/icons/development.svg";
+export const mobileIcon = "/assets/images/icons/mobile.svg";
+export const designIcon = "/assets/images/icons/design.svg";
